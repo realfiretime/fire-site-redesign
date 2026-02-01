4199f6d28bef7670f81ed0500bb258685097a31d
FireTime <firetime@firetime.me>
2026-02-01 03:43:54 -0500

    fixed themes

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -74,7 +74,7 @@ const fonts: FontsConfig = {
 // default customization applied to the HTML in the main layout.tsx
 const style: StyleConfig = {
   theme: "system", // dark | light | system
-  neutral: "mint", // sand | gray | slate | custom
+  neutral: "mint", // sand | gray | slate | custom | mint
   brand: "orange", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
   accent: "red", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan | custom
   solid: "contrast", // color | contrast
