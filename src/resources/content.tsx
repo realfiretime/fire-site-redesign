917bddc08825244a3f7630f840554c68851ad979
FireTime <firetime@firetime.me>
2026-02-01 03:27:23 -0500

    fixed themes, added wordmarks, and changed abit of the about section

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -234,7 +234,7 @@ const blog: Blog = {
     path: "/blog",
     label: "Blog",
     title: "Writing about design and tech...",
-    description: `Read what ${person.name} has been up to recently`,
+    description: `What have I been doing, well thats for me to know and you to find out`,
     // Create new blog posts by adding a new .mdx file to app/blog/posts
     // All posts will be listed on the /blog route
 };
