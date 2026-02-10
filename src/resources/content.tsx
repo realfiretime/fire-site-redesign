92d2765b04077fd34e9e1ca4e989386e65519fd3
FireTime <firetime@firetime.me>
2026-02-09 20:52:15 -0500

    fixed gallery

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -312,55 +312,7 @@ const gallery: Gallery = {
       orientation: "horizontal",
     },
   ],
-=======
-    path: "/gallery",
-    label: "Gallery",
-    title: `Images and Backrounds – Made By ${person.name}`,
-    description: `A photo collection by ${person.name}`,
-    // Images by https://lorant.one
-    // These are placeholder images, replace with your own
-    images: [
-        {
-            src: "/images/gallery/horizontal-1.jpg",
-            alt: "image",
-            orientation: "horizontal",
-        },
-        {
-            src: "/images/gallery/vertical-4.jpg",
-            alt: "image",
-            orientation: "vertical",
-        },
-        {
-            src: "/images/gallery/horizontal-3.jpg",
-            alt: "image",
-            orientation: "horizontal",
-        },
-        {
-            src: "/images/gallery/vertical-1.jpg",
-            alt: "image",
-            orientation: "vertical",
-        },
-        {
-            src: "/images/gallery/vertical-2.jpg",
-            alt: "image",
-            orientation: "vertical",
-        },
-        {
-            src: "/images/gallery/horizontal-2.jpg",
-            alt: "image",
-            orientation: "horizontal",
-        },
-        {
-            src: "/images/gallery/horizontal-4.jpg",
-            alt: "image",
-            orientation: "horizontal",
-        },
-        {
-            src: "/images/gallery/vertical-3.jpg",
-            alt: "image",
-            orientation: "vertical",
-        },
-    ],
+
 >>>>>>> origin/main
 };
 
